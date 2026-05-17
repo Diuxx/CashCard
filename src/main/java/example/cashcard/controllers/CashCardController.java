b268fafccbe08f93875f6b3c45cdfd5bc7611037
Diuxx <nicolasmarmot@gmail.com>
2026-05-17 11:12:41 +0200

    feat(controller): Update dependencies and add CashCardController with basic GET endpoint

---
+++ src/main/java/example/cashcard/controllers/CashCardController.java
@@ -0,0 +1,17 @@
+package example.cashcard.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<String> findById(@PathVariable String requestedId) {
+      return ResponseEntity.ok("{}");
+   }
+}
